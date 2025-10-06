775435af849941fa0d0d225679bed685a888b943
Yash954564 <yashgaikwad954564@gmail.com>
2025-10-06 10:47:24 +0530

    logo issue fixed

--- seo-optimizer-main/components/Header.tsx
+++ seo-optimizer-main/components/Header.tsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
+import Logo from '../assets/smartDatalogo.png'
 
 export const Header: React.FC = () => {
   return (
     <header className="bg-slate-900/80 backdrop-blur-sm sticky top-0 z-10 border-b border-slate-700">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <img src="./components/smartData-logo.png" alt="SmartData Logo"/>
+        {/* <img src="./components/smartData-logo.png" alt="SmartData Logo"/> */}
+        <img src={Logo} alt="SmartData Logo" />
         <h1 className="text-xl font-semibold tracking-tight text-gray-300 hidden sm:block">
           AI SEO & Performance Analyzer
         </h1>
